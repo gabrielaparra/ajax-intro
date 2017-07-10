ce4b733a46671d276783401fa4015494dd18d81d
Gabriela Parra <parragabriela@icloud.com>
2017-07-10 12:18:30 -0400

    added form to search and get pokemon by id number

--- js/get.js
+++ js/get.js
@@ -6,6 +6,21 @@ $(document).ready(() => {
     // Pokemon #42 is Golbat
   });
 
+  $('#pokemonSearch').submit((event) => {
+    // The 1st arg of any event callback (click, submit, scroll, etc.)
+    // is an 'Event' object.
+
+    // use the 'Event' objec to prevent the normal
+    // form submission page refresh
+    event.preventDefault();
+
+    //retrieve what the user typed in the form
+    const pokemonNumber = $('#pokemonId').val();
+
+    //call 'getPokemonInfo()' with the user's inputted number
+    getPokemonInfo(pokemonNumber);
+  });
+
 });
 
 function getPokemonInfo (pokemonId) {
@@ -16,11 +31,23 @@ function getPokemonInfo (pokemonId) {
     url: 'http://pokeapi.co/api/v2/pokemon/' + pokemonId,
     method: 'GET',
     //url and method define which API we'll be using
-    success: () => {},
-    error: () => {}
+    success: (responseFromApi) => {
+    // if successful, display some of the data on the screen
+    // (DOM manipulation)
+
+    //add the info to the <p> tag with the #pokeInfo id
+      $('#pokeInfo').html(`
+        ${responseFromApi.name}
+        <img src='${responseFromApi.sprites.front_default}'>
+      `);
+      //backticks use for string interpolation
+    },
+
+    error: (errorFromApi) => {
+      alert('Sorry, Pokemon data error.');
+      console.log(errorFromApi);
+    }
   });
-  // if successful, display some of the data on the screen
-  // (DOM manipulation)
 
   // if there's an error, show error feedback (DOM manipulation)
 }
