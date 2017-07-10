615bc8f1d2b19846ad76327c77e38af08cbc1adc
Gabriela Parra <parragabriela@icloud.com>
2017-07-10 14:57:11 -0400

    added post a character

--- js/post.js
+++ js/post.js
@@ -1,5 +1,42 @@
-console.log('yay');
-
 $(document).ready(() => {
-  console.log('body ready.')
+
+  $('#postWalle').click((event) => {
+    const walleInfo = {
+      name: 'WALL-E',
+      occupation: 'Waste Allocation Robot',
+      weapon: 'Head Laser'
+    };
+
+    postCharacterInfo(walleInfo);
+  });
+
 });
+
+function postCharacterInfo(newCharacterDetails) {
+  $.ajax({
+    url: 'https://ih-api.herokuapp.com/characters',
+    method: 'POST',
+    //'data' is only used when we need to send the API
+    //extra information
+    // data: {
+    //   name: '',
+    //   weapon: '',
+    //   occupation: ''
+    // },
+    data: newCharacterDetails,
+    //we can use this because newCharacterDetails is an object
+    //that already contains the data we want to post to the API
+    success: (responseFromApi) => {
+      $('#characterList').append(`
+        <li>
+          <h3> ${responseFromApi.name} </h3>
+          <p> id: ${responseFromApi.id} </p>
+        </li>
+      `);
+    },
+    error: (errorFromApi) => {
+      alert('Sorry, character POST error.');
+      console.log(errorFromApi);
+    }
+  });
+}
